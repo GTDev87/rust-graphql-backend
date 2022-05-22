ba6f9f24b0f3fdb2f65cd0fbc4917c1c77235011
João Rodrigues <joao.devmail@gmail.com>
2022-05-21 22:26:50 -0300

    feat: add actix-web

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use std::io;
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello World!")
+}
+
+#[actix_web::main]
+async fn main() -> io::Result<()> {
+    HttpServer::new(|| App::new().service(index))
+        .bind(("127.0.0.1", 4000))?
+        .run()
+        .await
 }
