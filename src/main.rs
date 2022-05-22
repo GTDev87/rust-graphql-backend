e7416ca2bac24de855400d04da0f2925a7057bda
João Rodrigues <joao.devmail@gmail.com>
2022-05-21 23:32:18 -0300

    feat: add juniper

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,51 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
-use std::io;
+use actix_cors::Cors;
+use actix_web::{
+    get, middleware, route,
+    web::{self, Data},
+    App, HttpResponse, HttpServer, Responder,
+};
+use actix_web_lab::respond::Html;
+use juniper::http::{graphiql::graphiql_source, GraphQLRequest};
+use std::{io, sync::Arc};
 
-#[get("/")]
-async fn index() -> impl Responder {
-    HttpResponse::Ok().body("Hello World!")
+mod schema;
+
+use crate::schema::{create_schema, Schema};
+
+/// GraphiQL playground UI
+#[get("/graphiql")]
+async fn graphql_playground() -> impl Responder {
+    Html(graphiql_source("/graphql", None))
+}
+
+/// GraphQL endpoint
+#[route("/graphql", method = "GET", method = "POST")]
+async fn graphql(st: web::Data<Schema>, data: web::Json<GraphQLRequest>) -> impl Responder {
+    let user = data.execute(&st, &()).await;
+    HttpResponse::Ok().json(user)
 }
 
 #[actix_web::main]
 async fn main() -> io::Result<()> {
-    HttpServer::new(|| App::new().service(index))
-        .bind(("127.0.0.1", 4000))?
-        .run()
-        .await
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+    // Create Juniper schema
+    let schema = Arc::new(create_schema());
+
+    log::info!("Starting HTTP server on port 4000");
+    log::info!("GraphiQL playground: http://localhost:4000/graphiql");
+
+    // Start HTTP server
+    HttpServer::new(move || {
+        App::new()
+            .app_data(Data::from(schema.clone()))
+            .service(graphql)
+            .service(graphql_playground)
+            .wrap(Cors::permissive())
+            .wrap(middleware::Logger::default())
+    })
+    .workers(2)
+    .bind(("127.0.0.1", 4000))?
+    .run()
+    .await
 }
