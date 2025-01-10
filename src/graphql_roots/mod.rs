fef93f0d8da3710c0da1e298b0ed53ae0765d1bb
Gregory Thompson <gregorythompson@MacBook-Pro.lan>
2025-01-10 14:56:47 -0500

    dded loader and moved it into todo schema folder

--- src/graphql_roots/mod.rs
+++ src/graphql_roots/mod.rs
@@ -1,4 +1,7 @@
 use juniper::{RootNode};
+use std::collections::HashMap;
+use std::sync::Arc;
+use dataloader::non_cached::Loader;
 
 mod queries;
 mod mutations;
@@ -10,14 +13,27 @@ pub fn create_schema() -> Schema {
     Schema::new(queries::QueryRoot {}, mutations::MutationRoot {}, subscriptions::SubscriptionRoot {})
 }
 
+#[derive(Clone)]
+pub struct Repository;
+
+impl Repository {
+    pub async fn load_todos_by_ids(&self, ids: &[crate::schema::todos::TodoId]) -> anyhow::Result<HashMap<crate::schema::todos::TodoId, crate::schema::todos::Todo>> { 
+        unimplemented!() 
+    }
+}
 
 #[derive(Clone)]
 pub struct Context {
+    repo: Repository,
+    todo_loader: crate::schema::todos::TodoLoader,
 }
+
+
 
 impl juniper::Context for Context {}
 
-
 pub fn create_context() -> Context {
-    Context {}
+    let repo = Repository;
+    let todo_loader = crate::schema::todos::new_todo_loader(repo.clone());
+    Context { repo, todo_loader }
 }
